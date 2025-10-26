322ba3f8af71828f2f1a48fed4660b4123db26a5
Arjunan1234 <arjunanvijay21@gmail.com>
2025-10-26 11:13:43 +0530

    portremoved

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,13 +6,14 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      port: process.env.PORT || 9000,
-      host: "0.0.0.0",
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true"
   }
 })
